313caa40ae6cd494fe3694e85d826b07bf9783b4
hichlink <1097279305@qq.com>
2015-11-22 11:21:33 +0800

    del

--- funion-admin-web/lib/deploy - 副本 - 副本.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo [INFO] Install jar to thirdparty repository.
-
-cd %~dp0
-
-mvn deploy:deploy-file -DgroupId=nu.xom -DartifactId=mysqljdbc -Dversion=1.2.5 -Dpackaging=jar -Dfile=xom-1.2.5.jar -Durl=http://192.168.1.20:8081/nexus/content/repositories/thirdparty -DrepositoryId=thirdparty
-
-pause
