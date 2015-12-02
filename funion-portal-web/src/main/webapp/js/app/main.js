cab4c0c4490f09a972cbb56f977c7cbb612af479
hichlink <1097279305@qq.com>
2015-12-02 18:58:07 +0800

    js

--- funion-portal-web/src/main/webapp/js/app/main.js
+++ funion-portal-web/src/main/webapp/js/app/main.js
@@ -1,12 +1,16 @@
 $(function() {
+	var flag = false;
 	$("#fetchCashBtn").click(function() {
+		if (flag)return;
+		flag = true;
 		$.ajax({
 			url : ctxPaths + '/fetchCash/fetch.do',
 			type : 'post',
 			data : {
-				cash : 1
+				cash : 100
 			},
 			success : function(data) {
+				flag = false;
 				if (data.success) {
 					alert('提取成功');
 				} else {
